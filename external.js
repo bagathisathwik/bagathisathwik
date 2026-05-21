f190e960aadc68bb2f71f402ed1247731ade9ef9
bagathisathwik <bagathisathwik@gmail.com>
2026-05-21 10:00:22 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,30 @@
+
+        let str="javascript"
+        console.log(str.length)
+
+        console.log(str.replace("java","type"))
+
+        console.log(str.replaceAll("a","t"))
+        
+        console.log(str.split(""))
+
+        console.log(str.split("a"))
+
+        console.log(str.toUpperCase())
+
+        console.log(str.toLowerCase(""))
+        
+        console.log(str.includes("js"));
+         
+        console.log(str.indexOf("a"));
+
+        console.log(str.lastIndexOf("a"));
+
+        console.log(str.charAt(0));
+
+        console.log(str.concat("is a programing"));
+
+        console.log(str.slice(4,7));
+
+
+        console.log("HELLO".toLowerCase());
